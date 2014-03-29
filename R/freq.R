02e73dbf955acebc834775a1947958a0748bc24d
Andreas Blaette <andreas.blaette@uni-due.de>
2014-03-29 22:20:26 +0100

    function frequencies added

--- R/freq.R
+++ R/freq.R
@@ -6,11 +6,11 @@
 #' @param Partition a partition object
 #' @param pAttribute pAttribute
 #' @export
-#' @rdname variation
-#' @name variation
-variation <- function(query, Partition, pAttribute=NULL) {
+#' @rdname frequencies
+#' @name frequencies
+frequencies <- function(query, Partition, pAttribute=NULL) {
   if (is.null(pAttribute)) pAttribute <- get("drillingControls", '.GlobalEnv')[['pAttribute']]
-  cpos <- driller:::.queryCpos(query, Partition, pAttribute)
+  cpos <- .queryCpos(query, Partition, pAttribute)
   attr <- paste(Partition@corpus, '.', pAttribute, sep='')
   explicit <- apply(cpos, 1, function(x) paste(cqi_cpos2str(attr, x[1]:x[2]), collapse=' '))
   Encoding(explicit) <- Partition@encoding
