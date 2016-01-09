6a29c2a016920cb7203cd9ecd731cbe035079616
Andreas Blaette <andreas.blaette@uni-due.de>
2016-01-09 17:04:31 +0100

    read-method for plprPartition and pressPartition

--- R/read_method.R
+++ R/read_method.R
@@ -1,7 +1,29 @@
 #' read an object
 #' 
+#' A html page will be generated and displayed in the viewer pane of RStudio.
+#' 
 #' @param .Object an object to be read
 #' @param ... further parameters
 #' @exportMethod read
 #' @rdname read-method
 setGeneric("read", function(.Object, ...) standardGeneric("read"))
+
+#' @rdname read-method
+setMethod("read", "plprPartition", function(.Object, meta=c("text_name", "text_party", "text_date")){
+  fulltextHtml <- html(.Object, meta=meta)
+  if(require("htmltools", quietly = TRUE)){
+    htmltools::html_print(fulltextHtml)  
+  } else {
+    warning("package htmltools required, but not available")
+  }
+})
+
+#' @rdname read-method
+setMethod("read", "pressPartition", function(.Object, meta=c("text_newspaper", "text_date")){
+  fulltextHtml <- html(.Object, meta=meta)
+  if(require("htmltools", quietly = TRUE)){
+    htmltools::html_print(fulltextHtml)  
+  } else {
+    warning("package htmltools required, but not available")
+  }
+})
