0f506ab4bcbcd6def02a7448edc10880b2744fa5
Andreas Blaette <andreas.blaette@uni-due.de>
2017-01-30 09:15:17 +0100

    cooccurrences local

--- R/cooccurrences_method.R
+++ R/cooccurrences_method.R
@@ -29,30 +29,30 @@ setGeneric("cooccurrences", function(.Object, ...){standardGeneric("cooccurrence
 
 #' @examples 
 #' \dontrun{
-library(polmineR)
-library(data.table)
-library(slam)
-PLPRBT <- Corpus$new("PLPRBT")
-PLPRBT$count(pAttribute = "word")
-PLPRBT$stat <- PLPRBT$stat[count >= 50]
-PLPRBT$stat <- PLPRBT$stat[-grep("^.*?(,|;|:|\\!|\\?|\\(|\\)|[|]|/|#|%|\\'|\\*|\\+).*?$", PLPRBT$stat[["word"]])]
-PLPRBT$stat <- PLPRBT$stat[-grep("^.*?\\d+.*?$", PLPRBT$stat[["word"]])]
-PLPRBT$stat <- PLPRBT$stat[-grep("^-", PLPRBT$stat[["word"]])]
-keep <- PLPRBT$stat[["word"]]
-PLPRBT_cooc <- cooccurrences("PLPRBT", keep = keep)
-rm(PLPRBT)
-PLPRBT_Matrix <- as.sparseMatrix(PLPRBT_cooc)
-rm(PLPRBT_cooc)
-PLPRBT_Matrix2 <- as(PLPRBT_Matrix, "dgTMatrix")
-rm(PLPRBT_Matrix)
-library(text2vec)
-GV <- GloVe$new(word_vectors_size = 50, vocabulary = keep, x_max = 10, learning_rate = .1) # in example learning_rate .25
-GV$fit(x = PLPRBT_Matrix2, n_iter = 20)
-vec <- GV$get_word_vectors()
-rm(GV)
-rm(PLPRBT_Matrix2)
-gc()
-foo <- coop::cosine(t(vec[1:20000,]))
+# library(polmineR)
+# library(data.table)
+# library(slam)
+# PLPRBT <- Corpus$new("PLPRBT")
+# PLPRBT$count(pAttribute = "word")
+# PLPRBT$stat <- PLPRBT$stat[count >= 50]
+# PLPRBT$stat <- PLPRBT$stat[-grep("^.*?(,|;|:|\\!|\\?|\\(|\\)|[|]|/|#|%|\\'|\\*|\\+).*?$", PLPRBT$stat[["word"]])]
+# PLPRBT$stat <- PLPRBT$stat[-grep("^.*?\\d+.*?$", PLPRBT$stat[["word"]])]
+# PLPRBT$stat <- PLPRBT$stat[-grep("^-", PLPRBT$stat[["word"]])]
+# keep <- PLPRBT$stat[["word"]]
+# PLPRBT_cooc <- cooccurrences("PLPRBT", keep = keep)
+# rm(PLPRBT)
+# PLPRBT_Matrix <- as.sparseMatrix(PLPRBT_cooc)
+# rm(PLPRBT_cooc)
+# PLPRBT_Matrix2 <- as(PLPRBT_Matrix, "dgTMatrix")
+# rm(PLPRBT_Matrix)
+# library(text2vec)
+# GV <- GloVe$new(word_vectors_size = 50, vocabulary = keep, x_max = 10, learning_rate = .1) # in example learning_rate .25
+# GV$fit(x = PLPRBT_Matrix2, n_iter = 20)
+# vec <- GV$get_word_vectors()
+# rm(GV)
+# rm(PLPRBT_Matrix2)
+# gc()
+foo <- coop::cosine(t(vec))
 #' 5000: 1 s
 #' 10 000: 4.4 s (764.2 Mb)
 #' 20 000: 20 s (3 Gb)
