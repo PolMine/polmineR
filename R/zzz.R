7d15797872bb63ef275ebdd0c1d81f96a8499c21
Andreas Blätte <andreas.blaette@uni-due.de>
2017-12-10 20:24:58 +0100

    moving forward

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,8 @@
   
   # adjust dataDir, if it has not yet been set
   cwbTmpDir <- file.path(libname, pkgname, "extdata", "cwb")
-  if (.Platform$OS.type == "windows"){
-    cwbTmpDir <- gsub("^[A-Z]?:?(.*)$", "\\1", cwbTmpDir)
-  }
+  if (.Platform$OS.type == "windows") cwbTmpDir <- gsub("^[A-Z]?:?(.*)$", "\\1", cwbTmpDir)
+  
   reutersTmpRegistry <- file.path(cwbTmpDir, "registry", "reuters")
   reutersTmpDataDir <- file.path(cwbTmpDir, "indexed_corpora", "reuters")
   # checking whether the registry file exists is necessary to circumvent a 
@@ -25,6 +24,7 @@
       polmineRPackageRegistry <- gsub("^[A-Z]?:?(.*)$", "\\1", polmineRPackageRegistry)
     }
     Sys.setenv("CORPUS_REGISTRY" = polmineRPackageRegistry)
+    print("foo")
     resetRegistry(registryDir = polmineRPackageRegistry, verbose = FALSE)
   }
   
