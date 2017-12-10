52db98fdc3185f0374d3e63fd2719da06b5fcdc0
Andreas Blätte <andreas.blaette@uni-due.de>
2017-12-10 19:11:09 +0100

    moving forward

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,9 @@
   
   # adjust dataDir, if it has not yet been set
   cwbTmpDir <- file.path(libname, pkgname, "extdata", "cwb")
+  if (.Platform$OS.type == "windows"){
+    cwbTmpDir <- gsub("^[A-Z]?:?(.*)$", "\\1", cwbTmpDir)
+  }
   reutersTmpRegistry <- file.path(cwbTmpDir, "registry", "reuters")
   reutersTmpDataDir <- file.path(cwbTmpDir, "indexed_corpora", "reuters")
   # checking whether the registry file exists is necessary to circumvent a 
