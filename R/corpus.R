29cf67865543f9bc191dd8d0eadcedb20cac68ae
Andreas Blaette <andreas.blaette@uni-due.de>
2019-03-07 17:23:26 +0100

    next

--- R/corpus.R
+++ R/corpus.R
@@ -398,12 +398,12 @@ setMethod("subset", "corpus", function(x, subset){
 
 
 setMethod("subset", "character", function(x, subset){
-  return(is(subset))
   expr <- substitute(subset)
   if (length(expr) == 1 && class(expr[[1]]) == "character" ) expr <- parse(text = subset)[[1]]
-  # expr <- substitute(date == "2009-11-10")
-  s_attr <- s_attributes(expr, corpus = corpus(x)) # get s_attributes present in the expression
+  s_attr <- s_attributes(expr, corpus = x) # get s_attributes present in the expression
+  return(s_attr)
 
+  
   max_attr <- s_attributes_stop_if_nested(corpus = x, s_attr = s_attr)
   df <- data.frame(struc = 0L:(max_attr - 1L))
   df <- .df_add_s_attributes(x = corpus(x), df = df, s_attr = s_attr)
