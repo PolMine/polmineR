5d2bc220a229eb9f2d11d9a723a870336e07b063
Andreas Blaette <andreas.blaette@uni-due.de>
2019-03-07 18:27:47 +0100

    next

--- R/corpus.R
+++ R/corpus.R
@@ -411,8 +411,9 @@ setMethod("subset", "character", function(x, subset, s_attributes = NULL){
   max_attr <- s_attributes_stop_if_nested(corpus = x, s_attr = s_attr)
   tab <- data.frame(struc = 0L:(max_attr - 1L))
   tab <- .df_add_s_attributes(x = corpus(x), df = tab, s_attr = s_attr)
-  r <- eval(expr, envir = df, enclos = parent.frame())
-  df_min <- df[r,]
+  return(tab)
+  r <- eval(expr, envir = tab, enclos = parent.frame())
+  df_min <- tab[r,]
   # r <- eval(expr, df)
   # df_min <- subset(df, expr)
   return(df_min)
