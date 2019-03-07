23ae6591d93b79a99a415ac67fa2aa6a464ba8e2
Andreas Blaette <andreas.blaette@uni-due.de>
2019-03-07 17:25:36 +0100

    next

--- R/corpus.R
+++ R/corpus.R
@@ -400,6 +400,7 @@ setMethod("subset", "corpus", function(x, subset){
 setMethod("subset", "character", function(x, subset){
   expr <- substitute(subset)
   if (length(expr) == 1 && class(expr[[1]]) == "character" ) expr <- parse(text = subset)[[1]]
+  return(s_attributes(x))
   s_attr <- s_attributes(expr, corpus = x) # get s_attributes present in the expression
   return(s_attr)
 
