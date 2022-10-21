7ed8fc92eaa317226be4657d3aafa4210bf3edaa
Yihui Xie <xie@yihui.name>
2022-10-21 10:16:10 -0500

    Read the css file from the package instead of the `markdown.HTML.stylesheet` option
    Because the option might be unset. It will be unset in a future version of the **markdown** package.

--- R/html.R
+++ R/html.R
@@ -78,7 +78,7 @@ setMethod("html", "character", function(object, corpus, height = NULL){
 
   css <- paste(
     c(
-      readLines(getOption("markdown.HTML.stylesheet")),
+      readLines(system.file("resources", "markdown.css", package = "markdown")),
       readLines(system.file("css", "tooltips.css", package = "polmineR"))
     ),
     collapse = "\n", sep = "\n"
