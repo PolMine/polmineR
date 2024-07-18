52d28f34c5a255960e1887cf7b5e0b519697cfae
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2024-07-18 23:18:36 +0200

    NA character

--- R/corpus.R
+++ R/corpus.R
@@ -105,8 +105,8 @@ setMethod("corpus", "character", function(
       encoding = cl_charset_name(corpus = .Object, registry = registry_dir),
       registry_dir = as.character(registry_dir),
       data_dir = as.character(data_dir),
-      info_file = if (file.exists(info_file)) as.character(info_file) else NA,
-      template = if (file.exists(template)) as.character(template) else NA,
+      info_file = if (file.exists(info_file)) as.character(info_file) else NA_character_,
+      template = if (file.exists(template)) as.character(template) else NA_character_,
       type = if ("type" %in% names(properties))
         properties[["type"]]
       else
