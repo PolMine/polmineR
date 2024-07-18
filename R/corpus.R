f36fcf16d5528812b51ebd512d8cdac6dd7df6bf
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2024-07-18 23:14:09 +0200

    no fs_path slots

--- R/corpus.R
+++ R/corpus.R
@@ -103,10 +103,10 @@ setMethod("corpus", "character", function(
       "corpus",
       corpus = .Object,
       encoding = cl_charset_name(corpus = .Object, registry = registry_dir),
-      registry_dir = registry_dir,
-      data_dir = data_dir,
-      info_file = if (file.exists(info_file)) info_file else path(NA),
-      template = if (file.exists(template)) template else path(NA),
+      registry_dir = as.character(registry_dir),
+      data_dir = as.character(data_dir),
+      info_file = if (file.exists(info_file)) as.character(info_file) else NA,
+      template = if (file.exists(template)) as.character(template) else NA,
       type = if ("type" %in% names(properties))
         properties[["type"]]
       else
